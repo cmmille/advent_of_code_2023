4cc1eb5aaada4276ac0c47c6629b02436d9753b1
cmmille <cmmiller.bio@gmail.com>
2023-12-05 13:03:07 -0800

    day 01 part 2

--- day_01/index.ts
+++ day_01/index.ts
@@ -1,4 +1,13 @@
 import { LineSearch } from "./line-search";
 
-const res = new LineSearch("./input.txt").processFile();
-console.log(res);
+// Part 1
+console.log("Part 1:");
+const partOneResult = new LineSearch("./input.txt").processFile();
+console.log(partOneResult);
+
+// Part 2
+console.log("Part 2:");
+const partTwoDemo = new LineSearch("./dummy.txt").processFile(true);
+console.log("Expected: 281, Got:", partTwoDemo);
+const partTwoResult = new LineSearch("./input.txt").processFile(true);
+console.log(partTwoResult);
--- day_01/line-search.ts
+++ day_01/line-search.ts
@@ -15,6 +15,37 @@ export class LineSearch {
     return { first: firstDigit, last: lastDigit };
   }
 
+  // replace each instance of a word with its number.
+  // look for the keys of mapValues in the line and replace them with the values
+  // Examples:
+  // oneabctwo => 1abc2
+  // twone => 21
+  preProcess(line: string): string {
+    const mapValues: { [key: string]: string } = {
+      one: "1",
+      two: "2",
+      three: "3",
+      four: "4",
+      five: "5",
+      six: "6",
+      seven: "7",
+      eight: "8",
+      nine: "9",
+    };
+
+    const preProcessedLine = Object.keys(mapValues).reduce(
+      (acc, key) =>
+        acc.replace(
+          new RegExp(key, "g"),
+          // first letter of key, value of key, last letter of key
+          `${key[0]}${mapValues[key]}${key[key.length - 1]}`
+        ),
+      line
+    );
+
+    return preProcessedLine;
+  }
+
   readLinesFromFile(): string[] {
     const data = fs.readFileSync(this.filePath, "utf8");
     const lines = data.split("\n");
@@ -30,10 +61,11 @@ export class LineSearch {
     return array.reduce((a, b) => a + b, 0);
   }
 
-  processFile() {
+  processFile(preProcess = false) {
     const lines = this.readLinesFromFile();
     const result = lines.map((line) => {
-      const { first, last } = this.findFirstLastDigit(line);
+      const currentLine = preProcess ? this.preProcess(line) : line;
+      const { first, last } = this.findFirstLastDigit(currentLine);
       return this.combineDigits(first, last);
     });
     return this.addArrayElements(result);
