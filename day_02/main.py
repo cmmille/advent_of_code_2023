c263c1768bf28476b553d9d5fdfd8a363e76c264
cmmille <cmmiller.bio@gmail.com>
2023-12-11 23:08:04 -0800

    refactor read file logic

--- day_02/main.py
+++ day_02/main.py
@@ -1,23 +1,24 @@
 from classes.game import Game
 from classes.cube_collection import CubeCollection
 
-def sum_valid_ids(file_path):
+def read_games(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
-        all_games = [Game(line) for line in lines]
-        bag = CubeCollection(12, 13, 14)
-        valid_ids = [game.id for game in all_games if game.is_possible(bag)]
-        sum_of_valid_ids = sum(valid_ids)
-        print(f"Sum of valid ids: {sum_of_valid_ids}")
+        return [Game(line) for line in lines]
+
+def sum_valid_ids(file_path):
+    all_games = read_games(file_path)
+    bag = CubeCollection(12, 13, 14)
+    valid_ids = [game.id for game in all_games if game.is_possible(bag)]
+    sum_of_valid_ids = sum(valid_ids)
+    print(f"Sum of valid ids: {sum_of_valid_ids}")
 
 def sum_minimum_cube_products(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        all_games = [Game(line) for line in lines]
-        minimum_cubes = [game.minimum_cubes() for game in all_games]
-        minimum_cube_products = [cube.product() for cube in minimum_cubes]
-        sum_of_minimum_cube_products = sum(minimum_cube_products)
-        print(f"Sum of minimum cube products: {sum_of_minimum_cube_products}")
+    all_games = read_games(file_path)
+    minimum_cubes = [game.minimum_cubes() for game in all_games]
+    minimum_cube_products = [cube.product() for cube in minimum_cubes]
+    sum_of_minimum_cube_products = sum(minimum_cube_products)
+    print(f"Sum of minimum cube products: {sum_of_minimum_cube_products}")
         
 def demo():
     print("Demo:")
